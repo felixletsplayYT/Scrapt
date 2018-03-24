3763510e1ef10918f4a6f3cc3da8d16fdd3ebb0a
Felix <felixletsplay@outlook.de>
2018-03-24 12:34:29 +0100

    Started "if"

--- src/com/rfeoi/scrapt/script/interpreter/Executer.java
+++ src/com/rfeoi/scrapt/script/interpreter/Executer.java
@@ -2,4 +2,5 @@ package com.rfeoi.scrapt.script.interpreter;
 
 public interface Executer {
     void execute(String spiritname, String command, String value);
+    String getValue(String spiritname, String command, String value);
 }
--- src/com/rfeoi/scrapt/script/interpreter/Interpreter.java
+++ src/com/rfeoi/scrapt/script/interpreter/Interpreter.java
@@ -6,38 +6,66 @@ import java.util.ArrayList;
 public class Interpreter {
     private ArrayList<String> lines;
     private Executer executer;
-    public Interpreter(File file, Executer executer){
+
+    public Interpreter(File file, Executer executer) {
         lines = new ArrayList<>();
         this.executer = executer;
     }
 
-    public void produce(){
-        for (String s : lines){
-            if (s.contains("->")){
-                String object = s.split("->")[0];
-                String command = s.split("->")[1].split("\\(")[0];
-                String args = s.split("\\(")[1].replace(")", "");
-                executer.execute(object, command, args);
-            }else{
-                if (s.contains("(")){
-                    String command = s.split("\\(")[0];
-                    String args = s.split("\\(")[1].replace(")", "");
-                    //TODO IF!
-                    switch(command){
-                        case "wait":
-                            try {
-                                Thread.sleep(Long.parseLong(args) * 1000);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
+    public void produce() {
+        boolean inIf = false;
+        boolean ifTrue = false;
+        for (String s : lines) {
+            if (s.equals("}")) {
+                inIf = false;
+                ifTrue = false;
+            } else {
+                if (!(inIf && !ifTrue)) {
+                    if (s.contains("->")) {
+                        String object = s.split("->")[0];
+                        String command = s.split("->")[1].split("\\(")[0];
+                        String args = s.split("\\(")[1].replace(")", "");
+                        executer.execute(object, command, args);
+                    } else {
+                        if (s.contains("(")) {
+                            String command = s.split("\\(")[0];
+                            String args = s.split("\\(")[1].replace(")", "");
+                            //TODO IF!
+                            switch (command) {
+                                case "wait":
+                                    try {
+                                        Thread.sleep(Long.parseLong(args) * 1000);
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                case "if":
+                                    inIf = true;
+                                    //TODO more testing options
+                                    if (args.contains("=")) {
+                                        String side1 = args.split("=")[0];
+                                        String side2 = args.split("=")[1];
+                                        if (side1.contains("->")){
+                                            side1 = getValue(side1);
+                                        }
+                                        if (side2.contains("->")){
+                                            side2 = getValue(side2);
+                                        }
+                                        if (side1.equalsIgnoreCase(side2)) ifTrue = true;
+                                    }
+                                    break;
                             }
-                            break;
-                        case "if":
-                            System.err.println("not included yet.");
-                            break;
-                    }
 
+                        }
+                    }
                 }
             }
         }
+    }
+    private String getValue(String cmd){
+        String side2_object = cmd.split("->")[0];
+        String side2_command = cmd.split("->")[1].split("\\(")[0];
+        String side2_args = cmd.split("\\(")[1].replace(")", "");
+        return executer.getValue(side2_object, side2_command, side2_args);
     }
 }
